ccbee676092d172eed2b27452a0044846203f512
Abdullah Riaz <therealskyz@gmail.com>
2024-05-11 20:47:09 -0400

    changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -20,7 +20,7 @@ export default async function AdminDashboard() {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <DashboardCard
         title="Sales"
-        subtitle={formatNumber(salesData.numOfSales)}
+        subtitle={`${formatNumber(salesData.numOfSales)} Orders`}
         body={formatCurrency(salesData.amount)}
       />
       <DashboardCard
