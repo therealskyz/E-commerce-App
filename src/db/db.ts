c86a78268fde08b374b8de368802eaafe069d647
Abdullah Riaz <therealskyz@gmail.com>
2024-05-22 17:25:17 -0400

    minor changes

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,30 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
-
-// declare global {
-//   var db: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
-
-// const db = globalThis.db ?? prismaClientSingleton();
-
-// export default db;
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalThis.db = db;
-// }
-
-// declare global {
-//     var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
-
-// const prisma = globalThis.prisma ?? prismaClientSingleton();
-
 export default prisma;
-
-// if (process.env.NODE_ENV !== "production") {
-//     globalThis.prisma = prisma;
-// }
