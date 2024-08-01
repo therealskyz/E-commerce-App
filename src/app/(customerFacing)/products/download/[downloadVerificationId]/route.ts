780d0a4b20f62ca6eaf4e6a0f82c42778c725abe
Abdullah Riaz <therealskyz@gmail.com>
2024-08-01 09:22:02 -0400

    Changes

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,3 +1,34 @@
-export default function GET() {
-  return "H!";
+import prisma from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+
+interface DownloadVerification {
+  params: {
+    downloadVerificationId: string;
+  };
+}
+export async function GET(
+  req: NextRequest,
+  { params: { downloadVerificationId } }: DownloadVerification
+) {
+  const data = await prisma.downloadVerification.findUnique({
+    where: {
+      id: downloadVerificationId,
+      expiresAt: {
+        gt: new Date(),
+      },
+    },
+    select: {
+      product: {
+        select: {
+          filePath: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  if (data == null) {
+    return new NextResponse
+  }
+  return new NextResponse("Hi");
 }
--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -18,15 +18,24 @@ export default async function SuccessPage({ searchParams }: SearchParamsProps) {
   const paymentIntent = await stripe.paymentIntents.retrieve(
     searchParams.payment_intent
   );
-  
+
+  if (paymentIntent.metadata.product == null) return notFound();
+
   const product = await prisma.product.findUnique({
     where: {
-      id: paymentIntent.metadata.productId,
+      id: paymentIntent.metadata.product,
     },
   });
 
+  if (product == null) return notFound();
+
+  const isSuccess = paymentIntent.status === "succeeded";
+
   return (
     <div className="max-w-5xl w-full mx-auto space-y-8">
+      <h1 className="text-4xl font-bold">
+        {isSuccess ? "Success!" : "Error!"}
+      </h1>
       <div className="flex gap-4 items-center">
         <div className="aspect-video flex-shrink-0 w-1/3 relative">
           <Image
@@ -44,65 +53,23 @@ export default async function SuccessPage({ searchParams }: SearchParamsProps) {
           <div className="line-clamp-3 text-muted-foreground">
             {product.description}
           </div>
+          <Button className="mt-4" size="lg" asChild>
+            {isSuccess ? (
+              <a
+                href={`/products/download/${await createDownloadVerification(
+                  product.id
+                )}`}
+              >
+                Download
+              </a>
+            ) : (
+              <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
+            )}
+          </Button>
         </div>
       </div>
     </div>
   );
-  // const paymentIntent = await stripe.paymentIntents.retrieve(
-  //   searchParams.payment_intent
-  // );
-
-  // if (paymentIntent.metadata.productId == null) return notFound();
-
-  // const product = await prisma.product.findUnique({
-  //   where: {
-  //     id: paymentIntent.metadata.productId,
-  //   },
-  // });
-
-  // if (product == null) return notFound();
-
-  // const isSuccess = paymentIntent.status === "succeeded";
-
-  // return (
-  //   <div className="max-w-5xl w-full mx-auto space-y-8">
-  //     <h1 className="text-4xl font-bold">
-  //       {isSuccess ? "Success!" : "Error!"}
-  //     </h1>
-  //     <div className="flex gap-4 items-center">
-  //       <div className="aspect-video flex-shrink-0 w-1/3 relative">
-  //         <Image
-  //           src={product.imagePath}
-  //           fill
-  //           alt={product.name}
-  //           className="object-cover"
-  //         />
-  //       </div>
-  //       <div>
-  //         <div className="text-lg">
-  //           {formatCurrency(product.priceInCents / 100)}
-  //         </div>
-  //         <h1 className="text-2xl font-bold">{product.name}</h1>
-  //         <div className="line-clamp-3 text-muted-foreground">
-  //           {product.description}
-  //         </div>
-  //         <Button className="mt-4" size="lg" asChild>
-  //           {isSuccess ? (
-  //             <a
-  //               href={`/products/download/${await createDownloadVerification(
-  //                 product.id
-  //               )}`}
-  //             >
-  //               Download
-  //             </a>
-  //           ) : (
-  //             <Link href={`/products/${product.id}/purchase`}>Try Again</Link>
-  //           )}
-  //         </Button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
 
 async function createDownloadVerification(productId: string) {
